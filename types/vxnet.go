28b8b9869d5725e05b67c5e314d1ef8b426f0a7a
Shijiang Wei <mountkin@gmail.com>
2017-01-22 23:44:32 +0800

    ip_network type should be a string
    Signed-off-by: Shijiang Wei <mountkin@gmail.com>

--- types/vxnet.go
+++ types/vxnet.go
@@ -12,13 +12,13 @@ type Vxnet struct {
 	CreateTime  time.Time `json:"create_time"`
 	InstanceIDs []string  `json:"instance_ids"`
 	Router      struct {
-		ID         string    `json:"router_id"`
-		Name       string    `json:"router_name"`
-		ManagerIP  net.IP    `json:"manager_ip"`
-		IPNetwork  net.IPNet `json:"ip_network"`
-		DynIPEnd   net.IP    `json:"dyn_ip_end"`
-		DynIPStart net.IP    `json:"dyn_ip_start"`
-		Mode       int       `json:"mode"`
+		ID         string `json:"router_id"`
+		Name       string `json:"router_name"`
+		ManagerIP  net.IP `json:"manager_ip"`
+		IPNetwork  string `json:"ip_network"`
+		DynIPEnd   net.IP `json:"dyn_ip_end"`
+		DynIPStart net.IP `json:"dyn_ip_start"`
+		Mode       int    `json:"mode"`
 	} `json:"router"`
 	ID string `json:"vxnet_id"`
 }
